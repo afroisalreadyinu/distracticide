2c3d7aead960d1c21a18ccc2960c0a847d75c424
Ulas Turkmen <ulas.turkmen@revolut.com>
2022-10-09 22:10:58 +0200

    Show alternative page with js and css, updated readme

--- firefox/distracticide.js
+++ firefox/distracticide.js
@@ -1,10 +1,19 @@
-function logURL(requestDetails) {
+let BlockedPages = ["news.ycombinator.com", "reddit.com", "twitter.com"];
+
+function checkURL(requestDetails) {
   if (["main_frame", "object"].includes(requestDetails.type)) {
-    console.log("Loading: " + requestDetails.url + " with type " + requestDetails.type);
-  }
+    let pageUrl = new URL(requestDetails.url);
+    for (const url of BlockedPages) {
+      if (url === pageUrl.hostname) {
+        browser.tabs.update(requestDetails.tabId, {"url": "/page.html"});
+        return {cancel: true};
+      }
+    }
+  };
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  logURL,
-  {urls: ["<all_urls>"]}
+  checkURL,
+  {urls: ["<all_urls>"]},
+  ['blocking']
 );
