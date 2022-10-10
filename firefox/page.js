9cdda15a25fcb809191ed7aba1279523853bc5d6
Ulas Turkmen <ulas.turkmen@revolut.com>
2022-10-10 23:07:03 +0200

    Added options, got it to work

--- firefox/page.js
+++ firefox/page.js
@@ -1,12 +1,42 @@
 var state = {};
 
-function updateText(saved) {
+function updateText() {
   const pageUrl = new URL(window.location.href);
   const blocked = pageUrl.searchParams.get("blocked");
   if (!blocked) return;
   const blockedUrl = new URL(blocked);
   document.getElementById("dest-hostname").textContent = blockedUrl.hostname;
   state['blockedUrl'] = blocked;
+}
+
+function appendToHostnames(hostname) {
+  const hostnameList = document.getElementById("hostname-list");
+  const li = document.createElement('li');
+  li.innerHTML = `<span class='hostname'>${hostname}</span> <a href="#" class="remove-link">Remove</a>`;
+  hostnameList.append(li);
+}
+
+function showHostnames() {
+  browser.storage.sync.get('blockedHosts').then((values) => {
+    const hostnames = values['blockedHosts'] || [];
+    for (const hostname of hostnames) {
+      appendToHostnames(hostname);
+    }
+  });
+}
+function removeHostname(event) {
+  if (!event.target.matches('.remove-link')) return false;
+  const hostname = event.target.parentElement.getElementsByClassName('hostname')[0].innerHTML;
+  browser.storage.sync.get('blockedHosts').then((values) => {
+    let hostnames = values['blockedHosts'] || [];
+    if (hostnames.includes(hostname)) {
+      hostnames = hostnames.filter(hn => hn !== hostname);
+      browser.storage.sync.set({blockedHosts: hostnames}).then(() => {
+        event.target.parentElement.remove();
+      });
+    };
+  });
+  return false;
 }
 
 function deactivateOnTab() {
@@ -21,7 +51,6 @@ function deactivateOnTab() {
   });
 }
 
-// TODO form field validation
 function addHostname(event) {
   const hostnameField = event.target.closest('form').getElementsByTagName("input")[0];
   const newHostname = hostnameField.value;
@@ -31,6 +60,7 @@ function addHostname(event) {
       blockedHosts.push(newHostname);
       browser.storage.sync.set({blockedHosts}).then(() => {
         hostnameField.value = "";
+        appendToHostnames(newHostname);
       });
     };
   });
@@ -39,8 +69,13 @@ function addHostname(event) {
 
 window.addEventListener('load', function(event) {
   updateText();
+  showHostnames();
   const button = document.getElementById("disable-button");
-  button.onclick = deactivateOnTab;
+  if (button) {
+    button.onclick = deactivateOnTab;
+  }
   const addHostnameButton = document.getElementById("add-hostname-button");
   addHostnameButton.onclick = addHostname;
+  const hostnameList = document.getElementById("hostname-list");
+  hostnameList.addEventListener('click', removeHostname);
 });
