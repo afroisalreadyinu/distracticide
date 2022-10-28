6ba7792888ea95c9da2ae658a118201cca2c834c
Ulas Turkmen <ulas.turkmen@revolut.com>
2022-10-28 19:14:47 +0200

    Moar tests

--- firefox/_test.js
+++ firefox/_test.js
@@ -1,14 +1,20 @@
 let assert = chai.assert;
 
 class FakeBrowser {
-  constructor(domains=[]) {
-    this.domains = domains;
+  constructor(domains=[], deactivatedOnTabs=[]) {
+    let fb = this;
     this.storage = {
       local: {
-        async get(key) { return {}; }
+        async get(key) { return { deactivatedOnTabs }; }
       },
       sync: {
-        async get(key) { return {}; }
+        async get(key) { return {blockedHosts: domains}; }
+      },
+    };
+    this.runtime = {
+      getURL(targetURL) {
+        fb.targetURL = targetURL;
+        return "REDIRECT";
       }
     };
   };
@@ -16,7 +22,7 @@ class FakeBrowser {
 
 describe("Background scripts", () => {
 
-  it("Skips requests that arenot main_from or object", async function() {
+  it("Skips requests that are not main_from or object", async function() {
     let [checkURL, _] = getHandlers(null);
     let retval = await checkURL({type: "", url: "https://twitter.com"});
     assert.deepEqual(retval, {});
@@ -28,4 +34,23 @@ describe("Background scripts", () => {
     let retval = await checkURL({type: "object", url: "https://twitter.com"});
     assert.deepEqual(retval, {});
   });
+
+  it("Checks domain and not URL", async function() {
+    const fakeBrowser = new FakeBrowser(["twitter.com", "blah.org"]);
+    let [checkURL, _] = getHandlers(fakeBrowser);
+    const url = "https://twitter.com/some/path";
+    let retval = await checkURL({type: "object", url: url});
+    assert.deepEqual(retval, {redirectUrl: "REDIRECT"});
+    assert.equal(fakeBrowser.targetURL, `page.html?blocked=${encodeURIComponent(url)}`);
+  });
+
+  it("Skips if deactivated on tab", async function() {
+    console.log("Now this one");
+    const fakeBrowser = new FakeBrowser(["twitter.com"], [72]);
+    let [checkURL, _] = getHandlers(fakeBrowser);
+    const url = "https://twitter.com/some/path";
+    let retval = await checkURL({type: "object", url: url, tabId: 72});
+    assert.deepEqual(retval, {});
+  });
+
 });
